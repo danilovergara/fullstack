84a83d96eaefb80249570aabfe5f142cc7291746
Danilo Vergara <danilo.vergara32@gmail.com>
2018-08-16 01:49:52 -0300

    Frontend is working. It loads all the needed info through the REST API

--- techk/apps/base/urls.py
+++ techk/apps/base/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
+
 from apps.base import views
 
 urlpatterns = [
-	url(r'^books/$', views.book_list),
-	url(r'^books/(?P<id>[0-9]+)/$', views.book_detail),
-	url(r'^categories/$', views.category_list),
-	url(r'^categories/(?P<id>[0-9]+)/$', views.category_detail),
-	url(r'^scraper/$', views.scrape_list)
+	url(r'^api/books/$', views.book_list),
+	url(r'^api/categories/(?P<id>[0-9]+)/books/', views.book_list),
+	url(r'^api/books/(?P<id>[0-9]+)/$', views.book_detail),
+	url(r'^api/categories/$', views.category_list),
+	url(r'^api/categories/(?P<id>[0-9]+)/$', views.category_detail),
+	url(r'^api/scraper/$', views.scrape_list),
+	url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
